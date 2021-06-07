a11ef3fb21bc05855f9f72ee5bdf9cb3542c5cb6
jreleaserbot <jreleaser@kordamp.org>
2021-06-07 21:44:31 +0200

    app v1.0.5-SNAPSHOT

---
+++ app_snapshot.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 8+
+//REPOS jitpack
+//DEPS com.github.jlengrand:app:main-SNAPSHOT
+
+public class app_snapshot {
+    public static void main(String... args) throws Exception {
+        io.helidon.examples.quickstart.se.Main.main(args);
+    }
+}
